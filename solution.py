d873cfb692738b322935023beba5aad74f5f818f
hy072586 <98764128+hy072586@users.noreply.github.com>
2022-04-15 17:47:54 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -139,7 +139,7 @@ stddev = 0
 for i in lst:
         stddev += (i - packet_avg) ** 2
         stddev = math.sqrt((stddev / len(lst)))
-    vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
+        vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
     return vars
 
 if __name__ == '__main__':
