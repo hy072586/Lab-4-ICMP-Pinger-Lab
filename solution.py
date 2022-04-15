3b4694210ffe6ca3e28f68dd39ab29e911cac5b3
hy072586 <98764128+hy072586@users.noreply.github.com>
2022-04-15 19:51:43 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -133,8 +133,8 @@ def ping(host, timeout=1):
     for i in lst:
         stddev += (i - packet_avg) ** 2
         stddev = math.sqrt((stddev / len(lst)))
-       vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round(packet_max, 8)),str(round(stdev(stdev_var), 8))]
-    return vars
+        vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round_packet_max, 8), str(round(stdev(stdev_var), 8))]
+        return vars
 
 if __name__ == '__main__':
     ping("google.co.il")
