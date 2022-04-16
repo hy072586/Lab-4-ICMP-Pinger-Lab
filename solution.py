4ccb360c46c659f7dc7c10132a008b72b5611499
hy072586 <98764128+hy072586@users.noreply.github.com>
2022-04-15 20:45:33 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -128,8 +128,8 @@ def ping(host, timeout=1):
     packet_min = min(lst)
     packet_max = max(lst)
     packet_avg = sum(lst) / len(lst)
-    stddev = 0
-    stddev = math.sqrt((stddev / len(lst)))
+    stdev_var = variance(lst)
+    stdev = math.sqrt(stdev_var)
     vars = [str(round(packet_min, 8)), str(round(packet_avg, 8)), str(round_packet_max, 8),
             str(round(stdev(stdev_var), 8))]
     return vars
